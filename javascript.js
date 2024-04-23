d71392e2805463bbe9bf497bafae972ccd3e771e
Franco <franco.venezia@hotmail.com>
2024-04-23 16:34:25 -0300

    Arreglado un poco para que este en español y un poco centrado

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
 let ganaste = "";
 let perdiste = "";
 let empate = "";
+const PIEDRA = "piedra";
+const PAPEL = "papel";
+const TIJERAS = "tijeras";
 let puntuacion = 0;
 let puntuacionComputadora = 0;
 
@@ -10,9 +13,10 @@ const piedra = document.getElementById("piedra");
 const papel = document.getElementById("papel");
 const tijeras = document.getElementById("tijeras");
 const container = document.getElementById("container");
+const contenedorImagen = document.getElementById("contenedorImagen");
+const h1Resultado = document.createElement("h1");
 const pPuntuacion = document.createElement("p");
 const pPuntuacionComputadora = document.createElement("p");
-const h1Resultado = document.createElement("h1");
 const imagenxd = document.createElement("img");
 // Definicion de elementos
 
@@ -20,11 +24,14 @@ const imagenxd = document.createElement("img");
 function computerPlay(){
     
     let x = Math.floor(Math.random()* 3);
-    if (x===0){
-        return "rock"
-    }else if(x===1){
-        return "paper"
-    }else return "scissors"
+    switch(x){
+        case 0:
+            return "piedra"
+        case 1:
+            return "papel"
+        default:
+            return "tijeras"
+    }
 }
 // Randomizador del juego (Computadora)
 
@@ -53,14 +60,14 @@ const cuandoEmpatas = () =>{
 
 // Chequea si ganaste o perdiste y te pone la respectiva imagen
 const ganarPerder = () =>{
-    if(puntuacion == 5){
+    if(puntuacion == 3){
         puntuacion = 0;
         puntuacionComputadora = 0;
         pPuntuacion.textContent = `Tu puntuacion es de ${puntuacion}`;
         pPuntuacionComputadora.textContent = `La puntuacion de tu oponente es de ${puntuacionComputadora}`;
         return imagenxd.src="Ganaste.png";
     }
-    else if (puntuacionComputadora == 5){
+    else if (puntuacionComputadora == 3){
         puntuacion = 0;
         puntuacionComputadora = 0;
         pPuntuacion.textContent = `Tu puntuacion es de ${puntuacion}`;
@@ -74,30 +81,25 @@ const ganarPerder = () =>{
         
 // Juega una ronda --------
 function playRound(playerSelection, computerSelection){
-    let r = "rock";
-    let p = "paper";
-    let s = "scissors";
-    x = playerSelection.toLowerCase();
-    y = computerSelection;
-    ganaste = `¡Ganaste!, ${x} gana contra ${y}`;
-    perdiste = `¡Perdiste!, ${x} pierde contra ${y}`;
-    empate = `¡Empataste!, tu ${x} y su ${y} son iguales`;
-    
-        if (x == y){
-            cuandoEmpatas();}
-        else if (x == r && y == p){ 
-            cuandoPerdes();}
-        else if (x == r && y == s){
-            cuandoGanas();}
-        else if (x == p && y == r){
-            cuandoGanas();}
-        else if (x == p && y == s){
-            cuandoPerdes;}
-        else if (x == s && y == r){
-            cuandoPerdes();}
-        else if (x == s && y == p){
-            cuandoGanas();}
-}
+    ganaste = `¡Ganaste!, ${playerSelection} gana contra ${computerSelection}`;
+    perdiste = `¡Perdiste!, ${playerSelection} pierde contra ${computerSelection}`;
+    empate = `¡Empataste!, tu ${playerSelection} y su ${computerSelection} son iguales`;
+
+        if (playerSelection == computerSelection)cuandoEmpatas();
+        
+        if (playerSelection == PIEDRA){ 
+            if (computerSelection == TIJERAS) cuandoGanas();
+            else cuandoPerdes()
+        }
+        if (playerSelection == PAPEL){
+            if (computerSelection == PIEDRA) cuandoGanas();
+            else cuandoPerdes();
+        }
+        if (playerSelection == TIJERAS){
+            if (computerSelection == PAPEL) cuandoGanas();
+            else cuandoPerdes();
+        }
+    }
 // Juega una ronda --------
 
 
@@ -117,15 +119,15 @@ function playRound(playerSelection, computerSelection){
 
         
 // Botones para jugar
-piedra.addEventListener("click", () => playRound("rock", computerPlay()));
-papel.addEventListener("click", () => playRound("paper", computerPlay()));
-tijeras.addEventListener("click", () => playRound("scissors", computerPlay()));
+piedra.addEventListener("click", () => playRound("piedra", computerPlay()));
+papel.addEventListener("click", () => playRound("papel", computerPlay()));
+tijeras.addEventListener("click", () => playRound("tijeras", computerPlay()));
 // Botones para jugar
 
 // Mete hijos al container
+container.appendChild(h1Resultado);
 container.appendChild(pPuntuacion);
 container.appendChild(pPuntuacionComputadora);
-container.appendChild(h1Resultado);
-container.appendChild(imagenxd);
+contenedorImagen.appendChild(imagenxd);
 // Mete hijos al container
 
